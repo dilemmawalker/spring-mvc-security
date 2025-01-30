27cb29882c36e88810eaaad078a3b4e9784f6cef
Yash Handa <yashhanda500@gmail.com>
2025-01-30 20:31:22 +0530

    udpated with spring validation through custom validation form

--- 01-spring-boot-mvc-security/src/main/java/com/dilemmawalker/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 01-spring-boot-mvc-security/src/main/java/com/dilemmawalker/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.dilemmawalker.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,5 +33,23 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //ensures all requests are authenticated & only then passed
+        http.authorizeHttpRequests(configurer ->
+                configurer.anyRequest().authenticated())
+                //form login helps to create a custom login form which is helpful in login process
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                //url which is used to autheticate the user(no controller needed, Spring Rocks :)
+                                .loginProcessingUrl("/authenticateTheUser")
+                                //permits users to see uptill this point without authentication
+                                .permitAll()
+                );
+
+        return http.build();
     }
 }
