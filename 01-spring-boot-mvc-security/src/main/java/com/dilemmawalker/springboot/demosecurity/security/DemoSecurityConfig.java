612ba9de632a81f0b5a9d7c3897c43fa34c616da
Yash Handa <yashhanda500@gmail.com>
2025-02-01 16:08:05 +0530

    removed hardcoded values & rather added real users to DB & access via JDBC

--- 01-spring-boot-mvc-security/src/main/java/com/dilemmawalker/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 01-spring-boot-mvc-security/src/main/java/com/dilemmawalker/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,36 +3,47 @@ package com.dilemmawalker.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+//    @Bean
+//    InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+//
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}1234")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}1234")
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("f")
+//                .password("{noop}1234")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
+
+
+
+//    we are able to directly inject JdbcUserDetailsManager  & access tables with exact same names provided
+//    by Spring Security by default.
+//    Table name: users & authorities are accessible directly.
     @Bean
-    InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}1234")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}1234")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("f")
-                .password("{noop}1234")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource  ){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
